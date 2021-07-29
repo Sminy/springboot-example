cd60178f12150baee44aa7100be2f40422767198
Sminy <821603537@qq.com>
2021-07-29 16:51:06 +0800

    GC demo

---
+++ springboot-javabase-demo/src/main/java/base/gc/GCDemo.java
@@ -0,0 +1,34 @@
+package base.gc;
+
+import java.util.Random;
+
+/**
+ * serial:
+ * -Xms10m -Xmx10m -XX:+PrintGCDetails -XX:+PrintCommandLineFlags -XX:+UseSerialGC
+ * parnew:
+ * -Xms10m -Xmx10m -XX:+PrintGCDetails -XX:+PrintCommandLineFlags -XX:+UseParNewGC
+ * parallel:
+ * -Xms10m -Xmx10m -XX:+PrintGCDetails -XX:+PrintCommandLineFlags -XX:+UseParallelGC
+ * ParallelOld:
+ * -Xms10m -Xmx10m -XX:+PrintGCDetails -XX:+PrintCommandLineFlags -XX:+UseParallelOldGC
+ * SerialOld:
+ * -Xms10m -Xmx10m -XX:+PrintGCDetails -XX:+PrintCommandLineFlags -XX:+UseSerialOldGC
+ * G1:
+ * -Xms10m -Xmx10m -XX:+PrintGCDetails -XX:+PrintCommandLineFlags -XX:+UseG1GC
+ */
+public class GCDemo {
+    public static void main(String[] args) throws InterruptedException {
+
+        Random rand = new Random(System.nanoTime());
+
+        try {
+            String str = "Hello, World";
+            while (true) {
+                str += str + rand.nextInt(Integer.MAX_VALUE) + rand.nextInt(Integer.MAX_VALUE);
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+
+    }
+}
